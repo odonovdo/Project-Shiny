fa6871036b3343d200994cc9a241658a84b76b46
odonovdo <odonovdo@gmail.com>
2015-07-28 03:12:55 +0100

    Commit

--- server.R
+++ server.R
@@ -3,45 +3,75 @@ library(shiny)
 library(ggplot2)
 
 shinyServer(function(input, output) {
-   
+      
+      AgentsSkill <- reactive({
+            Agents <- as.data.frame(rbind(c("Experienced",input$NoE,1.0),
+                                          c("Average",input$NoA,0.8),
+                                          c("Novice",input$NoN,0.6)))
+            names(Agents) <- c("Expertise","Available","Competency")
+            Agents[,2:3] <- lapply(Agents[,2:3],function(x)as.numeric(as.character(x)))
+            transform(Agents,Productivity =(Available* Competency))
+      })
+      
+      CallSimul <- reactive({
+            Bin <- list()
+            CallDur <- list()
+            for (j in 1:15){
+                  Calls <-  rpois(16,(input$N*abs(rnorm(input$N,0,                                                        as.numeric(input$p)/100)))/16)
+                  # Duration of Calls (Min)
+                  D <- vector()
+                  D <- sample(10:30,sum(Calls),replace = TRUE)
+                  
+                  #Total Call load per Period (30 min)
+                  d <- vector()
+                  for (i in 1:length(Calls)){
+                        d[i] <- sum(D[1:Calls[i]])
+                        D <- D[-c(1:Calls[i])]
+                  }
+                  Bin[[j]] <- d
+            }
+                  do.call(cbind,Bin)
+                  })
+      
       output$AgentPlot <- renderPlot({
-            AgentsSkill <- as.data.frame(table(sample(c("Expert","Average","New"),
-                                    input$Agents,replace = T,
-                                    prob = c(0.3,0.5,0.2))))
-            names(AgentsSkill) <- c("Expertise","Available")
-            plot1 <- ggplot(AgentsSkill,aes(Expertise,Available,fill=Expertise))+
+            
+            plot1 <- ggplot(AgentsSkill(),aes(Expertise,Available,fill=Expertise))+
                   geom_bar(stat="identity")+theme_classic()+
                   ggtitle("Staff Type Available")
             plot1
-            })
-    
-            output$distPlot <- renderPlot({
-                  Bin <- list()
-                  for (j in 1:10){
-                        Calls <-  rpois(16,(input$N*abs(rnorm(input$N,0,0.05)))/16)+1
-                        # Duration of Calls (Min)
-                        D <- vector()
-                        D <- sample(5:10,sum(Calls),replace = TRUE)
-                        #Total Call load per Period (30 min)
-                        d <- vector()
-                        for (i in 1:length(Calls)){
-                              d[i] <- sum(D[1:Calls[i]])
-                              D <- D[-c(1:Calls[i])]
-                        }
-                        Bin[[j]] <- d
-                  }
-                  Average <- do.call(cbind,Bin)
-                  PeriodAvg <- rowMeans(Average)
-                  
+      })
+      
+      output$distPlot <- renderPlot({
+            
+            PeriodAvg <- rowMeans(CallSimul(),na.rm = TRUE)
+            
             CallVol <- as.data.frame(cbind(Period=seq_along(PeriodAvg),
                                            Volume=PeriodAvg))
+            Data <- AgentsSkill()
+            StaffSkill <- sum(Data[,2]*Data[,3],na.rm = T)
             
             plot2 <- ggplot(CallVol,aes(Period,Volume))+
                   geom_line()+
-                  geom_hline(yintercept=input$Agents*30,colour="red")+
-                  geom_hline(yintercept=mean(CallVol$Volume),colour="Green")+
-                  theme_classic()+ggtitle("Call Volume Demand per 30 min Interval")
-            
+                  geom_hline(y=StaffSkill*30,color="red")+
+                  ylim(0,NA)+
+                  theme_classic()+
+                  ggtitle("Demand vs Capacity (per 30 min Intervals)")
             plot2
       })
+      
+      output$summary <- renderPrint({
+            summary(rowMeans(CallSimul(),na.rm = TRUE)/sum((AgentsSkill()[,4]*30)))
       })
+      output$table <- renderTable({
+            StaffAv <- AgentsSkill()
+            StaffAv[,c(1,4)]
+            })
+      output$ProdPlot <- renderPlot({
+            StaffAv <- AgentsSkill()
+           barplot(StaffAv[,4]*30)
+      })
+      
+      
+      
+      
+})
--- ui.R
+++ ui.R
@@ -4,27 +4,40 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Call Centre Metrics"),
+  titlePanel("Example of Call Arrival rates for a Call Centre"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("Agents",
-                  "Number of Agents:",
-                  min = 1,
-                  max = 15,
-                  value = 5),
       sliderInput("N",
                   "Number of Customers:",
                   min = 100,
                   max = 15000,
-                  value = 5000)),
+                  value = 5000,step = 250,
+                  animate = TRUE),
       
-
+      numericInput("NoA","No. Average Staff",
+                   1,
+                   min=0,max=9,step = 1),
+      
+      numericInput("NoE","No. Experienced Staff",
+                   1,
+                   min=0,max=9,step = 1),
+      numericInput("NoN","No. Novice Staff",
+                   1,
+                   min=0,max=9,step = 1),
+      
+      selectInput("p","Probability of a Customer Calling %",c(0.1,0.5,1,5,10),selected = 1),
+      submitButton("Update View")),
+      
     mainPanel(
-      plotOutput("distPlot"),
-      plotOutput("AgentPlot")
-      
+          tabsetPanel(
+                tabPanel("Plot", plotOutput("distPlot",width = "100%"), 
+                         plotOutput("AgentPlot",width = "80%")), 
+                tabPanel("Summary", verbatimTextOutput("summary")), 
+                tabPanel("Table", tableOutput("table"),plotOutput("ProdPlot"))
+          )
     )
+   
   )
 ))
